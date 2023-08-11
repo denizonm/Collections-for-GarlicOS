ae219b9ccfadc4e550a8722d10cfef0ee1c88508
Deniz Onmuş <85248647+denizonm@users.noreply.github.com>
2023-08-11 14:35:41 +0300

    Add files via upload

---
+++ Roms/COLLECTIONS/_Clear Recents.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+mv /mnt/mmc/Saves/CurrentProfile/lists/content_history.lpl /mnt/mmc/Saves/CurrentProfile/lists/content_history.lpl.bak
+mv /mnt/SDCARD/Saves/CurrentProfile/lists/content_history.lpl /mnt/SDCARD/Saves/CurrentProfile/lists/content_history.lpl.bak
